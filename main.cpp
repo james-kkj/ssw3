d10d2a860995f7e6b8e69c55b9ed142f1e2e6e2c
kangjoo <topfeeling03@naver.com>
2018-11-04 00:48:31 -0700

    get input

--- main.cpp
+++ main.cpp
@@ -1,23 +1,54 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+//#include <library.h>
 
 using namespace std;
 
+
 int main() {
-//리소스 등록
-	string ResourceInput;
-	getline(cin, ResourceInput);
-	while (getline(cin, ResourceInput)) {
+//get resource.dat
+	//class library _library;
 
+	ifstream ResourceFile("resource.dat");
+	if (ResourceFile.fail()) {
+		cout << "resource open error." << endl;
+		return 1;
 	}
-//
-
-	//라이브러리에 출력함수 여덟개 만들어 놓고 메인문에서
-	//그 객체 실행시키면 알아서 그중 하나 출력하도록
+	string ResourceInput;
+	string _ResourceName, _ResourceType;
+	getline(ResourceFile, ResourceInput);
+	while (getline(ResourceFile, ResourceInput)) {
+		_ResourceName = ResourceInput.substr(0, ResourceInput.find("\t"));
+		_ResourceType = ResourceInput.substr(ResourceInput.find("\t"));
+		//_library.AddResource(ResourceName, ResourceType);
+		cout << _ResourceName << "1" << _ResourceType << endl;
+	}
+	
+//get input.dat
 	cout << "Op_#\tReturn_code\tDescription" << endl;
-	string input;
-	while (getline(cin, input)) {
 
-		//cout << input << endl;
+	ifstream InputFile("input.dat");
+	if (InputFile.fail()) {
+		cout << "input open error." << endl;
+		return 1;
+	}
+	string Input;
+	string Date, ResourceType, ResourceName, Operation, MemberType, MemberName;
+	getline(InputFile, Input);
+	while (getline(InputFile, Input)) {
+		Date = Input.substr(0, Input.find("\t"));
+		Input = Input.substr(Input.find("\t") + 1);
+		ResourceType = Input.substr(0, Input.find("\t"));
+		Input = Input.substr(Input.find("\t") + 1);
+		ResourceName = Input.substr(0, Input.find("\t"));
+		Input = Input.substr(Input.find("\t") + 1);
+		Operation = Input.substr(0, Input.find("\t"));
+		Input = Input.substr(Input.find("\t") + 1);
+		MemberType = Input.substr(0, Input.find("\t"));
+		Input = Input.substr(Input.find("\t") + 1);
+		MemberName = Input;
+		
+		cout << Date << "1" << ResourceType << "1" << ResourceName << "1" << Operation << "1" << MemberType << "1" << MemberName << endl;
 	}
 }
